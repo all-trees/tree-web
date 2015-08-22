8dcb0169e36336be0f867b4a2d48df066b4f8316
dvberkel <daan.v.berkel.1980@gmail.com>
2015-08-22 09:56:18 +0200

    Test ApplicationContext configuration

---
+++ src/main/java/nl/dvberkel/context/ContextConfiguration.java
@@ -0,0 +1,50 @@
+package nl.dvberkel.context;
+
+import nl.dvberkel.dyck.Builder;
+import nl.dvberkel.dyck.Checker;
+import nl.dvberkel.svg.SvgTreeTransformer;
+import nl.dvberkel.tree.TreeTransformer;
+import nl.dvberkel.tree.Configuration;
+import org.springframework.context.annotation.Bean;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+
+import static nl.dvberkel.tree.Configuration.configuration;
+
+@org.springframework.context.annotation.Configuration
+public class ContextConfiguration {
+    @Bean
+    public Builder createBuilder() {
+        return new Builder(createChecker());
+    }
+
+    @Bean
+    public Checker createChecker() {
+        return new Checker("(", ")");
+    }
+
+    @Bean
+    public TreeTransformer createTreeTransformer() {
+        return new TreeTransformer(createConfiguration());
+    }
+
+    private Configuration createConfiguration() {
+        return configuration()
+                .withNodeRadius(20)
+                .withLeafRadius(3)
+                .withPadding(5);
+    }
+
+    @Bean
+    public SvgTreeTransformer createSvgTreeTransformer() {
+        return new SvgTreeTransformer(createConfiguration());
+    }
+
+    @Bean
+    public Transformer createTransformer() throws TransformerConfigurationException {
+        TransformerFactory factory = TransformerFactory.newInstance();
+        return factory.newTransformer();
+    }
+}
---
+++ src/test/java/nl/dvberkel/context/ApplicationContextTest.java
@@ -0,0 +1,65 @@
+package nl.dvberkel.context;
+
+import nl.dvberkel.dyck.Builder;
+import nl.dvberkel.dyck.Checker;
+import nl.dvberkel.svg.SvgTreeTransformer;
+import nl.dvberkel.tree.TreeTransformer;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.xml.transform.Transformer;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class ApplicationContextTest {
+    private ApplicationContext context;
+
+    @Before
+    public void createApplicationContext() {
+        context = new AnnotationConfigApplicationContext(ContextConfiguration.class);
+    }
+
+    @Test
+    public void shouldBeCreatedFromAConfiguration() {
+        assertThat(context, is(notNullValue()));
+    }
+
+    @Test
+    public void shouldHaveAChecker() {
+        Checker checker = context.getBean(Checker.class);
+
+        assertThat(checker, is(notNullValue()));
+    }
+
+    @Test
+    public void shouldHaveABuilder() {
+        Builder builder = context.getBean(Builder.class);
+
+        assertThat(builder, is(notNullValue()));
+    }
+
+    @Test
+    public void shouldHaveATreeTransformer() {
+        TreeTransformer transformer = context.getBean(TreeTransformer.class);
+
+        assertThat(transformer, is(notNullValue()));
+    }
+
+    @Test
+    public void shouldHaveASvgTreeTransformer() {
+        SvgTreeTransformer transformer = context.getBean(SvgTreeTransformer.class);
+
+        assertThat(transformer, is(notNullValue()));
+    }
+
+    @Test
+    public void shouldHaveATransformer() {
+        Transformer transformer = context.getBean(Transformer.class);
+
+        assertThat(transformer, is(notNullValue()));
+    }
+}
